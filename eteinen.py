59e510a3175c0b57f81b6f7e7bce51dcc5fd62db
Jesse Raspi <tdk.upper@gmail.com>
2015-03-26 13:24:27 +0200

    added setup for gpio

--- eteinen.py
+++ eteinen.py
@@ -1,2 +1,8 @@
-#valoautomaatio
-#tästä tulee paras
+import RPi.GPIO as GPIO
+
+#pin setup
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(13, GPIO.IN) #used to read sension detection
+GPIO.setup(16, GPIO.OUT) #controls relay
+
