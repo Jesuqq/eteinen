2bf3b4b968ffcf79f022391d29b1730f509a569b
Jesse Raspi <tdk.upper@gmail.com>
2015-03-26 14:18:15 +0200

    testi

--- eteinen.py
+++ eteinen.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 
+MOTION = 13
+RELAY = 16
 #pin setup
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(13, GPIO.IN) #used to read sension detection
-GPIO.setup(16, GPIO.OUT) #controls relay
+GPIO.setup(MOTION, GPIO.IN) #used to read sension detection
+GPIO.setup(RELAY, GPIO.OUT) #controls relay
 
+#this is test for git
